068f34abbeb63b97282c084f8ed64a5d953f68a2
Wang Yaoshen <wangyaoshen@bgi.com>
2019-09-29 22:41:37 +0800

    build in binutils-build

--- install/gnu/binutils.sh
+++ install/gnu/binutils.sh
@@ -6,6 +6,19 @@ source=${GNUmirror}/${pkgname}/${pkgname}-${pkgver}.tar.xz
 mkdir -p $HOME/src
 cd $HOME/src
 
-wget -m ${source}
-tar avxf $source && cd $(dirname $(tar -tf $source|head -n1)) \
-&& ./configure --prefix=$LOCAL && make -j 6 && make check && make install
+if [ !-e ${source} ];then
+	wget -m ${source}
+fi
+tar avxf $source && cd $(dirname $(tar -tf $source|head -n1)) 
+mkdir -p ${pkgname}-build
+cd ${pkgname}-build
+echo cd ${pkgname}-build
+../configure \
+--prefix=$LOCAL \
+--enable-lto \
+--with-isl=$LOCAL \
+--with-gmp=$LOCAL \
+--with-mpfr=$LOCAL \
+--with-mpc=$LOCAL \
+&& make -j 6 && make install
+# make check
