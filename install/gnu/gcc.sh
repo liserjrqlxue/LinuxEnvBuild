b8f10bf28a54beef941efb41585182ec3160ba8d
Wang Yaoshen <wangyaoshen@bgi.com>
2019-09-30 06:09:55 +0800

    enable-lto for gcc

--- install/gnu/gcc.sh
+++ install/gnu/gcc.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
-gnuMirror=mirrors.ustc.edu.cn/gnu
-pkgver=9.2.0
 pkgname=gcc
-source=${gnuMirror}/${pkgname}/${pkgname}-${pkgver}/${pkgname}-${pkgver}.tar.xz
+pkgver=9.2.0
+source=${GNUmirror}/${pkgname}/${pkgname}-${pkgver}/${pkgname}-${pkgver}.tar.xz
 mkdir -p $HOME/src
 cd $HOME/src
 wget -m ${source}
 tar avxf $source
-cd $(tar -tf $source|head -n1)
+cd $(tar -tf $source|head -n1|cut -f 1 -d '/')
 mkdir -p ${pkgname}-build
 cd ${pkgname}-build
 # update mpc <= mpfr <= gmp
-../configure --prefix=$LOCAL/${pkgname}-${pkgver}  -enable-languages=all --with-gmp=$LOCAL --with-mpfr=$LOCAL --with-mpc=$LOCAL \
+# update binutils for --enable-lto
+# update isl
+../configure \
+--prefix=$LOCAL/${pkgname}-${pkgver} \
+--enable-languages=all \
+--enable-lto \
+--with-gmp=$LOCAL \
+--with-mpfr=$LOCAL \
+--with-mpc=$LOCAL \
+--with-isl=$LOCAL \
 && make -j 6 \
 && make install
-
