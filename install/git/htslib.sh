71201ceb5359cb56ec55fd1d7901b59d36027a6b
Wang Yaoshen <wangyaoshen@bgi.com>
2019-09-29 13:51:11 +0800

    move htslib.sh to git subdir

---
+++ install/git/htslib.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+pkgname=htslib
+pkgauthor=samtools
+pkgtype=github
+
+source=github.com/${pkgauthor}/${pkgname}
+go get ${source}
+cd $(go env GOPATH)/src/${source}
+autoheader
+# update autoconf
+autoconf -Wno-syntax
+./configure --prefix=$LOCAL && make -j 6 && make check && make install
