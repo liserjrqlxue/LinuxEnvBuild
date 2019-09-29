c42271134c0bb1c49fd9dbe5d7ec67f4e13f0f85
Wang Yaoshen <wangyaoshen@bgi.com>
2019-09-29 18:15:07 +0800

    more enable for pcre

--- install/pcre.sh
+++ install/pcre.sh
@@ -5,6 +5,17 @@ source=ftp.pcre.org/pub/${pkgname}/${pkgname}-${pkgver}.tar.bz2
 
 mkdir -p $HOME/src
 cd $HOME/src
-wget -m $source ||  exit 1
+if [ ! -e $source ];then
+	wget -m $source ||  exit 1
+fi
 tar avxf $source && cd $(tar -tf $source|head -n1|cut -f 1 -d '/') &&
-./configure --prefix=$LOCAL && make -j 6 && make test && make install
+./configure \
+--prefix=$LOCAL \
+--enable-unicode-properties \
+--enable-pcre16 \
+--enable-pcre32 \
+--enable-jit \
+--enable-pcregrep-libz \
+--enable-pcregrep-libbz2 \
+--enable-pcretest-libreadline \
+&& make -j 6 && make test && make install
