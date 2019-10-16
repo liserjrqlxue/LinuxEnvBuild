6f5311fe898a6e3497de2fc6f3859b0a84973301
Wang Yaoshen <wangyaoshen@bgi.com>
2019-10-16 15:53:40 +0800

    install pkg-config

---
+++ install/git/pkg-config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+pkgname=pkg-config
+pkgauthor=freedesktop
+pkgtype=anongit
+
+source=$pkgtype.${pkgauthor}.org/${pkgname}
+git clone git://$source $source
+cd $(go env GOPATH)/src/${source}
+./autogen.sh && ./configure --prefix=$LOCAL && make -j 6 && make install
