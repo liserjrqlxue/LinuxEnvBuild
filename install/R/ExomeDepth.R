c062bea5c90ba708d5b77ebcef9cabd1fc488d6f
Wang Yaoshen <wangyaoshen@bgi.com>
2019-10-16 09:30:54 +0800

    install ExomeDepth

---
+++ install/R/ExomeDepth.R
@@ -0,0 +1,6 @@
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+#BiocManager::install("Biostrings")
+#BiocManager::install("Rsamtools")
+BiocManager::install("GenomicAlignments")
+install.packages("ExomeDepth")
