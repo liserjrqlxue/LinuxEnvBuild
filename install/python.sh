7ea14eb207e607275ab53efc06b91788980d6a0f
Wang Yaoshen <wangyaoshen@bgi.com>
2019-10-16 09:28:52 +0800

    fix python install

--- install/python.sh
+++ install/python.sh
@@ -5,11 +5,18 @@ source=www.python.org/ftp/python/${pkgver}/${pkgname}-${pkgver}.tar.xz
 
 mkdir -p $HOME/src
 cd $HOME/src
-wget -m $source
-tar avxf $source
-cd $(tar -tf $source|head -n1)
+if [ ! -e $source ];then
+	wget -m $source
+fi
+pkgdir=$(tar -tf $source|head -n1|cut -f 1 -d '/')
+if [ ! -d ${pkgdir} ];then
+	tar avxf $source
+fi
+cd ${pkgdir}
 # update gcc
-./configure --prefix=$LOCAL/${pkgname}-${pkgver} --enable-optimizations #--with-lto 
+# update libffi for _ctypes
+# update openssl for _ssl
+./configure --prefix=$LOCAL/${pkgname}-${pkgver} --enable-optimizations --with-lto 
 make -j 6
 make install
 #make test
