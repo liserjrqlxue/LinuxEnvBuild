eecfa9e27ce913ef65883f3cfbf97eda7de53510
Wang Yaoshen <wangyaoshen@bgi.com>
2019-10-16 13:22:29 +0800

    install automake

---
+++ install/gnu/automake.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+pkgname=automake
+pkgver=1.16
+source=${GNUmirror}/${pkgname}/${pkgname}-${pkgver}.tar.xz
+mkdir -p $HOME/src
+cd $HOME/src
+
+wget -m ${source}
+tar avxf $source
+cd $(tar -tf $source|head -n1|cut -f 1 -d '/')
+./configure --prefix=$LOCAL && make -j 6 && make check && make install
