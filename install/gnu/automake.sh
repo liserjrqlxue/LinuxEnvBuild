c442e17d18087d06707da3e38dd5a7f1d69db877
Wang Yaoshen <wangyaoshen@bgi.com>
2019-10-16 14:47:50 +0800

    skip make check

--- install/gnu/automake.sh
+++ install/gnu/automake.sh
@@ -8,4 +8,5 @@ cd $HOME/src
 wget -m ${source}
 tar avxf $source
 cd $(tar -tf $source|head -n1|cut -f 1 -d '/')
-./configure --prefix=$LOCAL && make -j 6 && make check && make install
+./configure --prefix=$LOCAL && make -j 6 && make install
+# make check
